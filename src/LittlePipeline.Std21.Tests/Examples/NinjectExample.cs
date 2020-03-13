52f96f96f74886fc03f5c1bcc07cefdcd194d1a0
Chris Loggins <christopher.loggins@gmail.com>
2020-03-13 10:48:51 -0500

    Added Standard 2.1 Library

---
+++ src/LittlePipeline.Std21.Tests/Examples/NinjectExample.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using LittlePipeline.Std21.Tests.Bits;
+using Ninject;
+using Ninject.Extensions.Factory;
+using Xunit;
+
+namespace LittlePipeline.Std21.Tests.Examples
+{
+    public class NinjectExample
+    {
+        [Fact]
+        public void ItCanUseOtherContainers()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<ITaskFactory>().ToFactory();
+
+            kernel.Bind<Increment>().ToSelf();
+            kernel.Bind<Square>().ToSelf();
+
+            kernel.Bind(typeof(IPipeline<>)).To(typeof(Pipeline<>));
+
+            var pipeline = kernel.Get<IPipeline<FirstTestSubject>>();
+
+            var subject = new FirstTestSubject();
+            pipeline.Subject(subject);
+            pipeline.Do<Increment>();
+            pipeline.Do<Increment>();
+            pipeline.Do<Square>();
+
+            subject.Value.Should().Be(4);
+        }
+    }
+}
