83ae9a44006328a5c2b1ed2b448ca665f98052cb
cloggins <christopher.loggins@gmail.com>
2024-05-08 09:24:42 -0500

    .NET 6 not needed anymore.
    Moving to just support .NET Standard.

--- src/LittlePipeline.Net6.Tests/Bits/FirstTestSubject.cs
+++
@@ -1,7 +0,0 @@
-namespace LittlePipeline.Net6.Tests.Bits
-{
-    public class FirstTestSubject
-    {
-        public int Value { get; set; }
-    }
-}
--- src/LittlePipeline.Net6.Tests/Bits/SecondTestSubject.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LittlePipeline.Net6.Tests.Bits
-{
-    public class SecondTestSubject
-    {
-        public int Value { get; set; }
-    }
-}
